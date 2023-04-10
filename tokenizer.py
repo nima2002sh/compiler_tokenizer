e782dc650e53ca85f9f4a2ca820436cbb8f10637
Nima Shahrokhi <nima2002sh@gmail.com>
2023-04-10 11:06:24 +0430

    reserved list

---
+++ tokenizer.py
@@ -0,0 +1,23 @@
+import ply.lex as lex
+
+class tokenizer:
+
+    reserved = {
+        'def' : 'DEF',
+        'var' : 'VAR',
+        'int' : 'INT',
+        'vector' : 'VECTOR',
+        'str' : 'STR',
+        'for' : 'FOR',
+        'to' : 'TO',
+        'length' : 'LENGTH',
+        'return' : 'RETURN',
+        'while' : 'WHILE',
+        'if' : 'IF',
+        'else' : 'ELSE',
+        'scan' : 'SCAN',
+        'print' : 'PRINT',
+        'list' : 'LIST',
+        'exit' : 'EXIT',
+        'null' : 'NULL'
+    }
