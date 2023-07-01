abfa557950f294cf16f3caa7d58a58cce720f570
Nima Shahrokhi <nima2002sh@gmail.com>
2023-07-01 21:01:05 +0330

    final commit

--- compiler.py
+++ compiler.py
@@ -1,80 +1,68 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
-class tokenizer(object):
+reserved = {
+    'def' : 'DEF',
+    'var' : 'VAR',
+    'int' : 'INT',
+    'vector' : 'VECTOR',
+    'str' : 'STR',
+    'for' : 'FOR',
+    'to' : 'TO',
+    'length' : 'LENGTH',
+    'return' : 'RETURN',
+    'while' : 'WHILE',
+    'if' : 'IF',
+    'else' : 'ELSE',
+    'scan' : 'SCAN',
+    'print' : 'PRINT',
+    'list' : 'LIST',
+    'exit' : 'EXIT',
+    'null' : 'NULL',
+    'iden' : 'IDEN'
+}
 
-    reserved = {
-        'def' : 'DEF',
-        'var' : 'VAR',
-        'int' : 'INT',
-        'vector' : 'VECTOR',
-        'str' : 'STR',
-        'for' : 'FOR',
-        'to' : 'TO',
-        'length' : 'LENGTH',
-        'return' : 'RETURN',
-        'while' : 'WHILE',
-        'if' : 'IF',
-        'else' : 'ELSE',
-        'scan' : 'SCAN',
-        'print' : 'PRINT',
-        'list' : 'LIST',
-        'exit' : 'EXIT',
-        'null' : 'NULL',
-    }
+literals = [ '[', ']', '?', ':', '+', '-', '*', '/', '%', '<', '>', '!', '(', ')', ';', '#', '=', '{', '}', ',']
 
-    literals = [ '[', ']', '?', ':', '+', '-', '*', '/', '%', '<', '>', '!', '(', ')', ';', '#', '=']
+tokens = ['NUMBER', 'STRING', 'ID', 'COMMENT', 'EQUALS', 'LEQ', 'HEQ', 'NOTEQ', 'OR', 'AND']+ list(reserved.values())
 
-    tokens = ['NUMBER', 'STRING', 'ID', 'COMMENT', 'EQUALS', 'LEQ', 'HEQ', 'NOTEQ', 'OR', 'AND']+ list(reserved.values())
-
-    t_EQUALS = r'=='
-    t_LEQ = r'<='
-    t_HEQ = r'>='
-    t_NOTEQ = r'!='
-    t_OR = r'\|\|'
-    t_AND = r'&&'
+t_EQUALS = r'=='
+t_LEQ = r'<='
+t_HEQ = r'>='
+t_NOTEQ = r'!='
+t_OR = r'\|\|'
+t_AND = r'&&'
 
 
-    def t_NUMBER(self, t):
-        r'[0-9]+'
-        t.value = int(t.value)
-        return t
-    
-    def t_ID(self, t):
-        r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.reserved.get(t.value,'ID') 
-        return t
+def t_NUMBER(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
 
-    t_STRING = r'(\'[^\'\r\n]*\')|("[^"\r\n]*")'
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID') 
+    return t
 
-    
-    t_ignore_COMMENT = r'\#.*'
-    
-    t_ignore  = ' \t'
+t_STRING = r'(\'[^\'\r\n]*\')|("[^"\r\n]*")'
 
-    def t_newline(self, t):
-        r'\n+'
-        t.lexer.lineno += len(t.value)
+t_ignore_COMMENT = r'\#.*'
 
-    def t_error(self, t):
-        print("Illegal character '%s'" % t.value[0])
-        t.lexer.skip(1)
+t_ignore  = ' \t'
 
-    def build(self,**kwargs):
-        self.lexer = lex.lex(module=self, **kwargs)
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-    def test(self, data):
-        self.lexer.input(data)
-        while True:
-             tok = self.lexer.token()
-             if not tok: 
-                 break
-             print(tok)
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
 
-t = tokenizer()
-t.build()
-data = open('input.txt', 'r')
-t.test(data.read())
+
+lexer = lex.lex()
+text_file = open("input.txt")
+data = text_file.read()
+lexer.input(data)
 
 
 class Table:
@@ -88,28 +76,29 @@ class Table:
         self.isAssignd = isAssignd
         self.isDefined = isDefined
 
-functionName = ['scan', 'print', 'list', 'length', 'exit']
+functionName = ['SCAN', 'PRINT', 'LIST', 'LENGTH', 'EXIT']
 info = {}
 allIden = []
 definedIden = []
-allReturnType = []
+allReturnType = {}
 cerFunction = ''
 cerReturnType = ''
+massage = []
 
-tScan = Table('scan', 'INT', [], True, 0)
-info['scan'] = tScan
+tScan = Table('SCAN', 'INT', [], True, 0)
+info['SCAN'] = tScan
 
-tPrint =  Table('print', 'INT', [], True, 0)
-info['print'] = tPrint
+tPrint =  Table('PRINT', 'INT', [], True, 0)
+info['PRINT'] = tPrint
 
-tList = Table('list', 'VECTOR', ['INT'], True, 1)
-info['list'] = tList
+tList = Table('LIST', 'VECTOR', ['INT'], True, 1)
+info['LIST'] = tList
 
-tLength = Table('length', 'INT', ['VECTOR'], True, 1)
-info['length'] = tLength
+tLength = Table('LENGTH', 'INT', ['VECTOR'], True, 1)
+info['LENGTH'] = tLength
 
-tExit = Table('exit', 'NULL', ['INT'], True, 1)
-info['exit'] = tExit
+tExit = Table('EXIT', 'NULL', ['INT'], True, 1)
+info['EXIT'] = tExit
 
 def checkDefined(t, line):
     if t in allIden:
@@ -131,7 +120,7 @@ def checkReturnType(t):
         return info[t].returnType
 
 precedence = (
-    ('left', '!', 'and', 'or', 'LEQ', 'HEQ', 'NOTEQ', 'EQUALS', '<', '>'),
+    ('left', '!', 'AND', 'OR', 'LEQ', 'HEQ', 'NOTEQ', 'EQUALS', '<', '>'),
     ('left', '=', ':'),
     ('left', '+', '-'),
     ('left', '*', '/'),
@@ -139,15 +128,14 @@ precedence = (
 )
 
 def p_prog(p):
-    """prog : func prog"""
-            
+    """prog :
+            | func prog"""
     if len(p) == 3:
         p[0] = [p[1], p[2]]
 
 def p_func(p):
-    """func : DEF type iden ( flist ) { body }
-            | DEF type iden ( flist ) RETURN expr ;"""
-    
+    """func : DEF type iden '(' flist ')' '{' body '}'
+            | DEF type iden '(' flist ')' RETURN expr ';'"""
     if p[7] == 'RETURN':
         checkFunction(p[3])
 
@@ -175,21 +163,21 @@ def p_func(p):
         p[0] = [p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9]]
     
 def p_body(p):
-    """body : stmt body"""
+    """body : 
+            | stmt body"""
     if len(p) == 3:
         p[0] = [p[1], p[2]]
 
 def p_stmt(p):
-    """stmt : expr ;
-            | defvar ;
-            | IF ( expr ) stmt
-            | IF ( expr ) stmt ELSE stmt
-            | WHILE ( expr ) stmt
-            | FOR ( iden = expr TO expr ) stmt 
-            | RETURN expr ;
-            | { body }
+    """stmt : expr ';'
+            | defvar ';'
+            | IF '(' expr ')' stmt
+            | IF '(' expr ')' stmt ELSE stmt
+            | WHILE '(' expr ')' stmt
+            | FOR '(' iden '=' expr TO expr ')' stmt 
+            | RETURN expr ';'
+            | '{' body '}'
             | func"""
-    
     if len(p) == 2:     # func
         p[0] = p[1]
         
@@ -222,8 +210,7 @@ def p_stmt(p):
 
 def p_defvar(p):
     """defvar : VAR type iden
-              | VAR type iden = expr"""
-
+              | VAR type iden '=' expr"""
     p[0] = [p[2], p[3]]
     
     if p[2] not in ['INT', 'VECTOR', 'STR', 'NULL']:
@@ -241,8 +228,7 @@ def p_defvar(p):
 def p_flist(p):
     """flist :
             | type iden
-            | type iden , flist"""
-            
+            | type iden ',' flist"""
     if len(p) == 1:
         p[0] = []
         
@@ -259,8 +245,7 @@ def p_flist(p):
 def p_clist(p):
     """clist :
             | expr
-            | expr , clist"""
-
+            | expr ',' clist"""
     if len(p) == 1:
         p[0] = []
         
@@ -270,40 +255,31 @@ def p_clist(p):
     else:
         p[0] = [p[1]] + p[3]
 
-def p_type(p):
-    """type : INT
-            | VECTOR
-            | STR
-            | NULL"""
-    p[0] = p[1]
-
 def p_expr(p):
-    """expr :=
-            | expr [ expr] 
-            | [ clist ] 
-            | expr ? expr : expr 
-            | expr + expr 
-            | expr - expr 
-            | expr * expr 
-            | expr / expr 
-            | expr % expr 
-            | expr > expr 
-            | expr < expr 
-            | expr == expr 
-            | expr >= expr 
-            | expr <= expr 
-            | expr != expr 
-            | expr || expr 
-            | expr && expr 
-            | ! expr 
-            | + expr 
-            | - expr 
+    """expr : expr '[' expr ']'
+            | '[' clist ']' 
+            | expr '?' expr ':' expr 
+            | expr '+' expr 
+            | expr '-' expr 
+            | expr '*' expr 
+            | expr '/' expr 
+            | expr '%' expr 
+            | expr '>' expr 
+            | expr '<' expr 
+            | expr EQUALS expr 
+            | expr HEQ expr 
+            | expr LEQ  expr 
+            | expr NOTEQ expr 
+            | expr OR expr 
+            | expr AND expr 
+            | '!' expr 
+            | '+' expr 
+            | '-' expr 
             | iden 
-            | iden = expr 
-            | iden ( clist ) 
-            | NUMBER 
-            | STR """
-            
+            | iden '=' expr 
+            | iden '(' clist ')' 
+            | num
+            | str """
     if len(p) == 6:     # expr ? expr : expr
         if p[1]:
             p[0] = p[3]
@@ -315,20 +291,20 @@ def p_expr(p):
         if p[2] == '(':     # iden ( clist )
             
             if not p[1] in functionName:
-                print('Error in line', p.lineno(1), ': dont have such a function!')
+                print('Error in line', p.lineno(2), ': dont have such a function!')
                 
             else:
                 fanction = info[p[1]]
                 
                 if not fanction.parametersNum == len(p[3]):
-                    print('Error in line', p.lineno(1), ': function', p[1], ' expects' ,info[p[1]].parametersNum, ' parameter but its given ', len(p[3]))
+                    print('Error in line', p.lineno(2), ': function', p[1], ' expects' ,info[p[1]].parametersNum, ' parameter but its given ', len(p[3]))
                     
                 else:
                     parm_list = info[p[1]].parametrs
                     
                     for i in range(len(parm_list)):
                         if checkReturnType(p[3][i]) != parm_list[j]  :
-                                print('Error in line' ,p.lineno(1) ,': wrong type for argument ', j+1, ' of function' ,p[1])
+                                print('Error in line' ,p.lineno(2) ,': wrong type for argument ', j+1, ' of function' ,p[1])
                                 
                 p[0] = p[1]
                 
@@ -351,34 +327,39 @@ def p_expr(p):
         if checkReturnType(p[1]) != 'NUMBER' and checkReturnType(p[1]) != 'STR':
             checkDefined(p[1], p.lineno(1))
     if not info.get(p[1]).isAssignd and not info.get(p[1]).isFunction:
-        print('Error in Line ',p.lineno(1) , ':  Variable ', p[1] ,'is used before being assigned')
+        print('Error in Line ',p.lineno(2) , ':  Variable ', p[1] ,'is used before being assigned')
 
 def p_iden(p):
     """iden : IDEN"""
     p[0] = p[1]
     allIden.append(p[0])
 
-
-def p_number(p):
+def p_num(p):
     """ num : NUMBER """
     p[0] = p[1]
     allIden.append(p[0])
 
-def p_string(p):
+def p_str(p):
     """ str : STR """
     p[0] = p[1]
     allIden.append(p[0])
 
 def p_error(p):
     if p:
-        print("Syntax error at token", p.type)
-        print("Syntax error at '%s'" % p.value)
-        print("line : '%s'" % p.lineno)
-        print(p)
-        parser.errok()
+         print("Syntax error at token", p.type, p.value)
+         parser.errok()
     else:
-        print("Syntax error at EOF")
+         print("Syntax error at EOF")
+
+def p_type(p):
+    """ type : INT
+            | NULL
+            | VECTOR"""
+    p[0] = p[1]
+         
+
 
 
 parser = yacc.yacc(debug=True)
-parser.parse(tracking=True)
+out = parser.parse(data)
+text_file.close()
